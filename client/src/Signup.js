916dfeac5562921cdbe6f30fd53852894dfc3116
meet11 <meet@rapidken.ai>
2021-06-06 13:51:22 +0530

    Revert "[Issue #1]:- Feature: New design for login/Signup pages"
    This reverts commit 953a6a403d92f29bb6de8610fd93d56369f08ab0.

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -11,7 +11,6 @@ import {
   FormHelperText,
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
-import AuthTemplate from "./components/Template/AuthTemplate";
 
 const Login = (props) => {
   const history = useHistory();
@@ -20,7 +19,6 @@ const Login = (props) => {
 
   const handleRegister = async (event) => {
     event.preventDefault();
-    console.log("Inside handle register")
     const username = event.target.username.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
@@ -39,74 +37,73 @@ const Login = (props) => {
   }
 
   return (
-    // <Grid container justify="center">
-    //   <Box>
-    //     <Grid container item>
-    //       <Typography>Need to log in?</Typography>
-    //       <Button onClick={() => history.push("/login")}>Login</Button>
-    //     </Grid>
-    //     <form onSubmit={handleRegister}>
-    //       <Grid>
-    //         <Grid>
-    //           <FormControl>
-    //             <TextField
-    //               aria-label="username"
-    //               label="Username"
-    //               name="username"
-    //               type="text"
-    //               required
-    //             />
-    //           </FormControl>
-    //         </Grid>
-    //         <Grid>
-    //           <FormControl>
-    //             <TextField
-    //               label="E-mail address"
-    //               aria-label="e-mail address"
-    //               type="email"
-    //               name="email"
-    //               required
-    //             />
-    //           </FormControl>
-    //         </Grid>
-    //         <Grid>
-    //           <FormControl error={!!formErrorMessage.confirmPassword}>
-    //             <TextField
-    //               aria-label="password"
-    //               label="Password"
-    //               type="password"
-    //               inputProps={{ minLength: 6 }}
-    //               name="password"
-    //               required
-    //             />
-    //             <FormHelperText>
-    //               {formErrorMessage.confirmPassword}
-    //             </FormHelperText>
-    //           </FormControl>
-    //         </Grid>
-    //         <Grid>
-    //           <FormControl error={!!formErrorMessage.confirmPassword}>
-    //             <TextField
-    //               label="Confirm Password"
-    //               aria-label="confirm password"
-    //               type="password"
-    //               inputProps={{ minLength: 6 }}
-    //               name="confirmPassword"
-    //               required
-    //             />
-    //             <FormHelperText>
-    //               {formErrorMessage.confirmPassword}
-    //             </FormHelperText>
-    //           </FormControl>
-    //         </Grid>
-    //         <Button type="submit" variant="contained" size="large">
-    //           Create
-    //         </Button>
-    //       </Grid>
-    //     </form>
-    //   </Box>
-    // </Grid>
-    <AuthTemplate type="signup" handleRegister={handleRegister}></AuthTemplate>
+    <Grid container justify="center">
+      <Box>
+        <Grid container item>
+          <Typography>Need to log in?</Typography>
+          <Button onClick={() => history.push("/login")}>Login</Button>
+        </Grid>
+        <form onSubmit={handleRegister}>
+          <Grid>
+            <Grid>
+              <FormControl>
+                <TextField
+                  aria-label="username"
+                  label="Username"
+                  name="username"
+                  type="text"
+                  required
+                />
+              </FormControl>
+            </Grid>
+            <Grid>
+              <FormControl>
+                <TextField
+                  label="E-mail address"
+                  aria-label="e-mail address"
+                  type="email"
+                  name="email"
+                  required
+                />
+              </FormControl>
+            </Grid>
+            <Grid>
+              <FormControl error={!!formErrorMessage.confirmPassword}>
+                <TextField
+                  aria-label="password"
+                  label="Password"
+                  type="password"
+                  inputProps={{ minLength: 6 }}
+                  name="password"
+                  required
+                />
+                <FormHelperText>
+                  {formErrorMessage.confirmPassword}
+                </FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid>
+              <FormControl error={!!formErrorMessage.confirmPassword}>
+                <TextField
+                  label="Confirm Password"
+                  aria-label="confirm password"
+                  type="password"
+                  inputProps={{ minLength: 6 }}
+                  name="confirmPassword"
+                  required
+                />
+                <FormHelperText>
+                  {formErrorMessage.confirmPassword}
+                </FormHelperText>
+              </FormControl>
+            </Grid>
+            <Button type="submit" variant="contained" size="large">
+              Create
+            </Button>
+          </Grid>
+        </form>
+      </Box>
+    </Grid>
   );
 };
 
--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,7 @@
 const Sequelize = require("sequelize");
-/*const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
-});*/
-const db = new Sequelize('messenger','meet','meet',{
-  host : 'localhost',
-  port : 5432,
-  dialect : 'postgres',
-  logging : false
 });
 
 module.exports = db;
